dc73c5f31f3b29ed867e4fa6f60f2ff1f96d6250
Maxi Ramos <maxramos497@gmail.com>
2024-08-25 12:06:56 -0300

    Primer commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+function contarVocales() {
+    let continuar = true;
+
+    while (continuar) {
+        let palabra = prompt("Ingresá una o varias palabras para conocer cuántas vocales contienen:");
+        let contador = 0;
+
+        for (let i = 0; i < palabra.length; i++) {
+            let letra = palabra[i].toLowerCase();
+
+            if (letra === 'a' || letra === 'e' || letra === 'i' || letra === 'o' || letra === 'u') {
+                contador++;
+            }
+        }
+
+        alert("La palabra '" + palabra + "' contiene " + contador + " vocales.");
+
+        // Preguntar si desea analizar otra palabra
+        continuar = confirm("¿Querés analizar otra palabra?");
+    }
+
+    alert("¡Gracias por utilizar el contador de vocales!");
+}
+
+contarVocales();
